667d3e32a56bfe91e4e0b6cbb4136925b6cd008b
NikolayP <nikipanovski@gmail.com>
2023-05-24 18:03:31 +0200

    Annotate GameItem properties with display names

--- DE_GamesCatalog/Models/GameItemModel.cs
+++ DE_GamesCatalog/Models/GameItemModel.cs
@@ -11,22 +11,36 @@ namespace DE_GamesCatalog.Models
     {
         public ObjectId _id { get; set; }
 
+        [Display(Name = "Name")]
         public string name { get; set; }
+
         [Url]
+        [Display(Name = "Image URL")]   // TODO edit to "Image" whenever image fetched itself will be shown (here??)
         public string imageURL { get; set; }
+
+        [Display(Name = "Genre")]
         public string genre { get; set; }   // enum possible?
+
+        [Display(Name = "Short description")]
         public string shortDescription { get; set; }
 
         /// -- Ratings: a Rating is an int between 0-5 that results in a customized display (such as stars).
         /// (ideally should be a float for accurate average values)
         /// int? = Nullable<int>. Default to null. This should correspond to a "no rating yet" representation.
         [Range(0, 5)]
+        [Display(Name = "Gameplay Density")]
         public int? gameplayDensity { get; set; } = null;
+
         [Range(0, 5)]
+        [Display(Name = "RNG Density")]
         public int? RNGDensity { get; set; } = null;
+
         [Range(0, 5)]
+        [Display(Name = "Amount of glitches")]
         public int? glitchesAmount { get; set; } = null;
+
         [Range(0, 5)]
+        [Display(Name = "Value for money")]
         public int? valueForMoney { get; set; } = null;
 
 
@@ -38,6 +52,7 @@ namespace DE_GamesCatalog.Models
         // In theory we could be less discriminatory and make the field
         // without a site affiliation(just pass any useful redirection URL), array, or at least add another one for YouTube.
         [Url]
+        [Display(Name = "Twitch Directory URL")]
         public string twitchDirectoryURL { get; set; }
     }
 }
