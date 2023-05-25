dd611f3ead7a0dbf0e6d7c2766a41f4e8c836712
NikolayP <nikipanovski@gmail.com>
2023-05-25 16:31:02 +0200

    Import & include bootstrat-star-rating plugin
    https://github.com/kartik-v/bootstrap-star-rating

---
+++ DE_GamesCatalog/wwwroot/js/star-rating-inputs.js
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DE_GamesCatalog.wwwroot.js
+{
+    public class star_rating_inputs
+    {
+    }
+}
